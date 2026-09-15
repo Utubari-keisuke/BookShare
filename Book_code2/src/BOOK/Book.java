7a352bfc0609b8a32971ff24c04f923a0c9d621a
Charles・S <liangyingliang767@gmail.com>
2026-09-15 22:25:51 +0900

    first commit: プロジェクト初期化

---
+++ Book_code2/src/BOOK/Book.java
@@ -0,0 +1,60 @@
+package BOOK;
+
+public class Book {
+	private int id;
+	private String title;
+	private boolean isborrowed;
+	private String borrower;
+	private int rating;
+	private String review;
+
+	public Book(int id, String title) {
+		this.id = id;
+		this.title = title;
+		this.isborrowed = false;
+		this.borrower = "なし"; //nullでもいいけど
+		this.rating = 0;
+		this.review = "未記入";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public boolean isBorrowed() {
+		return isborrowed;
+	}
+
+	public void setBorowed(boolean isborrowed) {
+		this.isborrowed = isborrowed;
+	}
+
+	public String getBorrower() {
+		return borrower;
+	}
+
+	public void setBorrower(String borrower) {
+		this.borrower = borrower;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public String getReview() {
+		return review;
+	}
+
+	public void setReview(String review) {
+		this.review = review;
+	}
+
+}
---
+++ Book_code2/src/Main/main.java
@@ -0,0 +1,55 @@
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<Book> books = new ArrayList<>();
+		int nextId = 1; 
+
+		
+		while (true) {
+			System.out.println("\n=== 技術書シェア＆レビュー管理 ===");
+			System.out.println("1. 本を登録する");
+			System.out.println("2. 本の一覧を見る");
+			System.out.println("3. 貸出/返却の切り替え");
+			System.out.println("4. 本の評価とレビューを書く");
+			System.out.println("5. 本を削除する");
+			System.out.println("6. 終了");
+			System.out.print("番号を選択してください: ");
+			
+			int menu = -1;
+			try {
+				menu = scanner.nextInt();
+				scanner.nextLine();  // 改行の読み飛ばし
+			} catch (InputMismatchException e) {
+				System.out.println("半角数字を入力してください。");
+				scanner.nextLine(); 
+				continue;
+			}
+
+			
+			
+			if(menu == 0) {
+				System.out.println("アプリを終了します。");
+				break;
+			}
+
+			
+			else if (menu == 1) {
+				System.out.print("本のタイトルを入力してください: ");
+				String title = scanner.nextLine();
+				
+				if (title.isEmpty()) {
+					System.out.println("タイトルが入力されていません。");
+					continue;
+				}
+				
+				Book newBook =new Book(nextId, title);
+				booklist.add(newBook);
+				System.out.println("『" + title + " 』を登録しました。(ID: " + nextId + ")");
+
+				nextId++;
+				}
